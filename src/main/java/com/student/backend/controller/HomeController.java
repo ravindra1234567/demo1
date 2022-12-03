f86083c47a0087e5b8cbd83c855e7856c124c066
ravindra1234567 <ravindrakushwahanwg@gmail.com>
2022-12-03 11:13:59 +0530

    first commit

---
+++ src/main/java/com/student/backend/controller/HomeController.java
@@ -0,0 +1,15 @@
+
+package com.student.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello Ravindra !!!";
+    }
+    
+}
